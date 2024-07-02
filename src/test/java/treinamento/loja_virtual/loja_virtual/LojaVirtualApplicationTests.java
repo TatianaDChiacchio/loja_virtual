56ad866a94f5d1098bf7f1467b179db029ad1755
TatianaChiacchio <tatachiacchio@gmail.com>
2024-07-02 11:48:54 -0300

    criação projto

---
+++ src/test/java/treinamento/loja_virtual/loja_virtual/LojaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package treinamento.loja_virtual.loja_virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
