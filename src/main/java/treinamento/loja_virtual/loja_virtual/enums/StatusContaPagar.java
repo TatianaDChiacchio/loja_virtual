3748df96e6f32ce6bd98c915bfa651bd076fa989
TatianaChiacchio <tatachiacchio@gmail.com>
2024-07-03 16:08:35 -0300

    Implementação de classes e enums

---
+++ src/main/java/treinamento/loja_virtual/loja_virtual/enums/StatusContaPagar.java
@@ -0,0 +1,28 @@
+package treinamento.loja_virtual.loja_virtual.enums;
+
+public enum StatusContaPagar {
+
+    COBRANCA("Pagar"),
+    VENCIDA("Vencida"),
+    ABERTA("Aberta"),
+    QUITADA("Quitada"),
+    ALUGUEL("Aluguel"),
+    FUNCIONARIO("Funcionário"),
+    NEGOCIADA("Renegociada");
+
+
+    private String descricao;
+
+    private StatusContaPagar(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return this.descricao;
+    }
+}
---
+++ src/main/java/treinamento/loja_virtual/loja_virtual/enums/StatusContaReceber.java
@@ -0,0 +1,25 @@
+package treinamento.loja_virtual.loja_virtual.enums;
+
+public enum StatusContaReceber {
+
+    COBRANCA("Pagar"),
+    VENCIDA("Vencida"),
+    ABERTA("Aberta"),
+    QUITADA("Quitada");
+
+
+    private String descricao;
+
+    private StatusContaReceber(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return this.descricao;
+    }
+}
